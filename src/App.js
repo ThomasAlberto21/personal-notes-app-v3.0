c5bf9c7dfaa49bc99b4aef538aafb1be78631592
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-04-25 09:35:50 +0700

    CARD NOTES

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Navbar from './Components/Navbar';
+import CardTodos from './Components/CardTodos';
 
 export default function App() {
   return (
     <div className='App'>
       <Navbar />
+      <div className='px-10'>
+        <h1 className='font-bold text-gray-900'>Catatan Anda</h1>
+        <CardTodos />
+      </div>
     </div>
   );
 }
---
+++ src/Components/CardTodos.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function CardNotes() {
+  return (
+    <div>
+      <div class='block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-900'>
+        <h5 class='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>
+          Noteworthy technology acquisitions 2021
+        </h5>
+        <p class='font-normal text-gray-700 dark:text-gray-400'>
+          Here are the biggest enterprise technology acquisitions of 2021 so
+          far, in reverse chronological order.
+        </p>
+      </div>
+    </div>
+  );
+}
