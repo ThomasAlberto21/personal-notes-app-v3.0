28499cad1453d29861a1bc1417ed924874dec3d8
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-04-25 10:06:39 +0700

    GET API SUCCESS

--- src/Components/CardNotes.jsx
+++ src/Components/CardNotes.jsx
@@ -1,17 +1,14 @@
 import React from 'react';
 
-export default function CardNotes() {
+export default function CardNotes(props) {
   return (
-    <div>
-      <div className='block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-900'>
-        <h5 className='mb-2 text-2xl font-bold tracking-normal text-gray-900 dark:text-white '>
-          Noteworthy technology acquisitions 2021
-        </h5>
-        <p className='font-normal text-gray-700 dark:text-gray-400'>
-          Here are the biggest enterprise technology acquisitions of 2021 so
-          far, in reverse chronological order.
-        </p>
-      </div>
+    <div className='block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-900'>
+      <h5 className='mb-2 text-2xl font-bold tracking-normal text-gray-900 dark:text-white '>
+        {props.title}
+      </h5>
+      <p className='font-normal text-gray-700 dark:text-gray-400'>
+        {props.body.substring(0, 100) + '...'}
+      </p>
     </div>
   );
 }
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,13 +1,40 @@
 import React from 'react';
 import CardNotes from './CardNotes';
+import Axios from 'axios';
 
 class Home extends React.Component {
+  state = {
+    notes: [],
+  };
+
+  componentDidMount() {
+    Axios.get('https://jsonplaceholder.typicode.com/posts').then((res) => {
+      this.setState({
+        notes: res.data,
+      });
+    });
+  }
+
   render() {
     return (
-      <div className='px-10'>
-        <h1 className='font-bold text-gray-900 text-2xl'>Catatan Anda</h1>
-        <CardNotes />
-      </div>
+      <>
+        <h1 className='font-bold text-gray-900 text-2xl flex justify-center'>
+          Catatan Anda
+        </h1>
+        <div className='flex justify-center'>
+          <div className='lg:grid lg:grid-cols-3 lg:gap-6 '>
+            {this.state.notes.map((Notes) => {
+              return (
+                <CardNotes
+                  key={Notes.id}
+                  title={Notes.title}
+                  body={Notes.body}
+                />
+              );
+            })}
+          </div>
+        </div>
+      </>
     );
   }
 }
