596b3b3e3013c968db377f579f1009625e5a7a7f
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-04-28 11:01:43 +0700

    button remove

--- src/Components/CardNotes.jsx
+++ src/Components/CardNotes.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 
 export default function CardNotes(props) {
   return (
-    <div className='block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-900 lg:mb-0 my-5'>
+    <div className='block max-w-sm px-6 pb-20 pt-5 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-900 lg:mb-0 my-5 relative'>
       <h5 className='mb-2 text-2xl font-bold tracking-normal text-gray-900 dark:text-white '>
         {props.title}
       </h5>
-      <p className='font-normal text-gray-700 dark:text-gray-400 mt-10'>
+      <p className='font-normal text-gray-700 dark:text-gray-400 mt-5'>
         {props.body.substring(0, 100) + '...'}
       </p>
+      <button className='bg-red-500 text-white px-3 py-2 mt-6 mb-4 rounded-md bottom-0 absolute font-semibold'>
+        Remove
+      </button>
     </div>
   );
 }
