143d126baaebe3a140924adf723c95e5b9a7e7ec
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-04-29 10:00:40 +0700

    .

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -29,6 +29,17 @@ class Home extends React.Component {
     });
   };
 
+  handleFormNotesChange = (e) => {
+    let formNotesNew = { ...this.state.formNotes }; //  mengekstrak nilai-nilai dari sebuah array atau objek
+    formNotesNew[e.target.name] = e.target.value; // 
+    this.setState(
+      {
+        formNotes: formNotesNew,
+      },
+      () => console.log(this.state.formNotes)
+    );
+  };
+
   componentDidMount() {
     this.getNotesToAPI(); // memnggil fungsi get untuk ditampilkan ke browser
   }
@@ -36,34 +47,32 @@ class Home extends React.Component {
   render() {
     return (
       <div>
-        <form className='bg-gray-900 lg:w-2/4 p-7 lg:m-auto lg:my-7 mx-5 my-5 rounded-lg '>
+        <form className='bg-gray-900 lg:w-2/4 md:w-2/3 p-7 lg:m-auto md:m-auto lg:my-7 md:my-7 mx-5 my-5 rounded-lg '>
           <div className='mb-6'>
-            <label
-              for='title'
-              className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
-            >
+            <label className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>
               Your Title
             </label>
             <input
+              name='title'
               type='text'
               id='title'
               className='bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
               placeholder='Input Your Tittle....'
+              onChange={this.handleFormNotesChange}
             />
           </div>
-          <div class='mb-6'>
-            <label
-              for='body'
-              className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
-            >
+          <div className='mb-6'>
+            <label className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>
               Your Notes
             </label>
             <textarea
+              name='body'
               type='text'
               id='body'
               rows={10}
               className='bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
               placeholder='Input Your Notes....'
+              onChange={this.handleFormNotesChange}
             />
           </div>
           <button
@@ -78,7 +87,7 @@ class Home extends React.Component {
         </h1>
 
         <div className='flex justify-center'>
-          <div className='lg:grid lg:grid-cols-3 lg:gap-5 mx-5 mb-5'>
+          <div className='lg:grid lg:grid-cols-3 md:flex  lg:gap-5 mx-5 mb-5'>
             {this.state.notes.map((Notes) => {
               return (
                 <CardNotes
